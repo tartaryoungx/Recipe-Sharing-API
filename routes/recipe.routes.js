6b0425cb01a9b49eb91c9e5f951e9e5f76affaff
Jonggun <tartaryoungx@gmail.com>
2026-05-27 17:35:24 +0700

    complete basic api recipe include fav and search

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -6,8 +6,10 @@ const { v4: uuidv4 } = require("uuid");
 
 
 router.post('/recipes', authMiddleware, async (req, res, next) => {
+    let conn;
     try {
-        const conn = getConn()
+        conn = getConn()
+        await conn.beginTransaction()
         const user_id = req.user.id;
         const {
             name,
@@ -32,13 +34,13 @@ router.post('/recipes', authMiddleware, async (req, res, next) => {
 
         const [results] = await conn.query("INSERT INTO recipes SET ?", recipeData);
 
-        let ingredientId
-
         for (const item of ingredients) {
             const [existingIngredients] = await conn.query(
                 'SELECT id from ingredients where name = ?', 
                 [item.name]
             );
+        let ingredientId
+
         if(existingIngredients.length === 0) {
             ingredientId  = uuidv4();
 
@@ -69,13 +71,16 @@ router.post('/recipes', authMiddleware, async (req, res, next) => {
 
         }
 
-    }
+    }   await conn.commit()
         res.status(201).json({
             message: "Successful",
             results,
         });
-
+        
     } catch (error) {
+        if (conn) {
+            await conn.rollback();
+        }
         next(error);
     }
 
@@ -95,8 +100,8 @@ router.get('/recipes', authMiddleware, async (req, res, next) => {
                 ON r.id = ri.recipe_id
             LEFT JOIN ingredients  i
                 ON i.id = ri.ingredient_id
-            GROUP BY r.id, r.name
             WHERE r.user_id = ?
+            GROUP BY r.id, r.name
         `, [user_id]);
         res.json({
             recipes: result,
@@ -105,6 +110,50 @@ router.get('/recipes', authMiddleware, async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+
+})
+
+
+router.get('/recipes/favorite', authMiddleware, async (req, res, next) => {
+    try {
+        const conn = getConn()
+        const user_id = req.user.id;
+        const [results] = await conn.query(`
+        SELECT r.id,
+        r.name 
+        FROM recipes r
+        join fav_recipes f
+        on r.id = f.recipe_id
+        join users u
+        on u.id = f.user_id
+        WHERE u.id = ?
+        `, [user_id]);
+
+        res.status(200).json({
+            results,
+        });
+    } catch (error) {
+        next(error);
+    }
+
+})
+
+
+router.get('/recipes/search', authMiddleware, async (req, res, next) => {
+    try {
+            const keyword = req.query.keyword
+            const conn = getConn()
+            const [result] = await conn.query(`
+                SELECT * from recipes
+                where name like ?
+                `, [`%${keyword}%`])
+
+            res.json({
+                result
+            })
+        } catch (error) {
+            next(error)
+        }
 
 })
 
@@ -193,5 +242,72 @@ router.delete('/recipes/:id', authMiddleware, async (req, res, next) => {
         next(error);
     }
 })
+
+router.post('/recipes/:id/favorite', authMiddleware, async (req, res, next) => {
+    try {
+        const conn = getConn()
+        const user_id = req.user.id;
+        const recipe_id = req.params.id;
+
+        const favdata = {
+            recipe_id, 
+            user_id
+        }
+
+        const [recipeCheck] = await conn.query(`
+            SELECT id 
+            FROM recipes
+            WHERE id = ?
+            `, [recipe_id]
+        );
+        if (recipeCheck.length === 0) {
+            return res.status(404).json({
+            message: "Recipe not found"
+        });
+        }
+
+        
+        const [results] = await conn.query(`
+        INSERT INTO fav_recipes SET ?
+        `, favdata
+        );
+
+        res.status(201).json({
+            message: "Successful",
+            results,
+        });
+
+    } catch (error) {
+        next(error);
+    }
+
+})
+
+router.delete('/recipes/:id/favorite', authMiddleware, async (req, res, next) => {
+    try {
+        const conn = getConn()
+        const user_id = req.user.id;
+        const recipe_id = req.params.id;
+        const [result] = await conn.query(`
+        DELETE FROM fav_recipes 
+        WHERE user_id = ? AND
+        recipe_id = ?
+        `, [user_id, recipe_id]);
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: "Recipe not found"
+            })
+        }
+        res.status(200).json({
+            message: "Recipe deleted",
+            result,
+        });
+    } catch (error) {
+        next(error);
+    }
+
+})
+
+
 
 module.exports = router
